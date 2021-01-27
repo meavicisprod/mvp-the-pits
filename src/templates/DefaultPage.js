7546d744a32ab0a4169b21f3f25fd5b92816685a
Althea Yi <alsery@protonmail.com>
2021-01-26 22:40:57 -0500

    fix/ adds missing BackgroundVideo import to DefaultPage

--- src/templates/DefaultPage.js
+++ src/templates/DefaultPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
+import BackgroundVideo from '../components/BackgroundVideo'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
 import SVGIcon from '../components/SVGIcon'
